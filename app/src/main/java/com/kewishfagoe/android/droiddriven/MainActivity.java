114dcd76968eb02498db21574927a05a6bd8aae4
Kewish Fagoe <virinfagoe@hotmail.com>
2016-02-21 16:46:13 -0300

    Bug Fix: Crash without FAB Fixed

--- app/src/main/java/com/kewishfagoe/android/droiddriven/MainActivity.java
+++ app/src/main/java/com/kewishfagoe/android/droiddriven/MainActivity.java
@@ -2,8 +2,6 @@ package com.kewishfagoe.android.droiddriven;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -21,14 +19,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     @Override
